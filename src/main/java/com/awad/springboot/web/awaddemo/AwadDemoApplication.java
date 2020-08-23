e84355406e9cc4aed3fb5cff2a9f6b34ddbe0f68
AwadYMohammed <awadyasirawad@gmail.com>
2020-08-22 23:51:28 -0400

    Importingan existing maven project into eclipse created from spring.io

---
+++ src/main/java/com/awad/springboot/web/awaddemo/AwadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.awad.springboot.web.awaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwadDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awad/springboot/web/awaddemo/AwadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awad.springboot.web.awaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
